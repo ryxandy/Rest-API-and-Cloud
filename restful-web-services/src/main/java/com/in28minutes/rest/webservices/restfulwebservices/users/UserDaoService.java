1fa03f996a408b01e993329face708a97bf2c8cb
Alexandre <ryxandy@hotmail.com>
2021-03-01 10:38:54 -0300

    Creating the Resourcers and some basic end points

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/users/UserDaoService.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/users/UserDaoService.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserDAO {
+public class UserDaoService {
 
 	private static List<User> users = new ArrayList<>();
 	
---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/users/UserResource.java
@@ -0,0 +1,22 @@
+package com.in28minutes.rest.webservices.restfulwebservices.users;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	//This class is made to deal with rest calls, where i will create the end points
+	
+	@Autowired
+	private UserDaoService service;
+	
+	
+	//Get all users
+	@GetMapping(path = "/users")
+	public List<User> findAll(){
+		return service.findAll();
+	}
+}
