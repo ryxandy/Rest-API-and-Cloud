dcd474ab002b913c7062a0502718d2ab2b36e417
Alexandre <ryxandy@hotmail.com>
2021-03-04 16:49:54 -0300

    Implementing internationalized responses

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,8 +1,14 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties.LocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -11,4 +17,20 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+	@Bean
+	public SessionLocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		
+	return localeResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource MessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		
+	return messageSource;
+	}
+	
 }
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/Controller.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/Controller.java
@@ -1,20 +1,28 @@
 package com.in28minutes.rest.webservices.restfulwebservices.controller;
 
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+	@Autowired
+	private MessageSource messageSource;
+	
+	
 	//Returning a string
 	@GetMapping(path = "/hello")
 	public String helloWorld() {
 		System.out.println("Entrou aqui! ");
 		return "Hello World";
 	}
-	
 	
 	//Returning a bean that will be converted to json
 	@GetMapping(path = "/hello-bean")
@@ -30,5 +38,9 @@ public class Controller {
 			return new  Bean(String.format("Hello world, %s", name));
 		}
 	
+	@GetMapping(path = "/hello-world-internationalized")	
+	public String HelloWorldInternationalized(@RequestHeader(name = "Accepted-Language", required = false ) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
+	}
 	
 }
